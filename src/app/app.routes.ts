f755b786bac4f4261e1819f0be06ff56da419701
Leonel <leonelpala234@gmail.com>
2024-08-14 17:17:44 -0300

    Pagina de auth formulario y ruteo de login y register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { LoginComponent } from './components/auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      { path: '', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+    ],
+  },
+];
