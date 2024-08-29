a27e1015aa98383fe657d21acf3c5274abce12d9
Tarek Daher <tarkxddd@gmail.com>
2024-08-29 14:08:49 -0300

    Landing page creada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,13 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { AuthComponent } from './pages/auth/auth.component';
 import { LoginComponent } from './components/auth/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'auth',
     component: AuthComponent,
