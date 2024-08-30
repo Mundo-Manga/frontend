5d991e360730bf9d6a42898d7f95abca45008260
Leonel <leonelpala234@gmail.com>
2024-08-30 03:20:59 -0300

    Landing page mejorada

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderShopComponent } from '../../components/shop/header-shop/header-shop.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderShopComponent, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
